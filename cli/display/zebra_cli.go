5cdd33c8745f727aae8e2e6411832705567b352a
Eva Achim <eva.achim@bison.howard.edu>
2022-07-29 13:10:07 -0400

    changed directory name from show to display to avoid stuttering
    Signed-off-by: Eva Achim <eva.achim@bison.howard.edu>

---
+++ cli/display/zebra_cli.go
@@ -0,0 +1,152 @@
+package display
+
+import (
+	"github.com/spf13/cobra"
+)
+
+/*
+Build a zebra inventory command line client:
+zebra show servers
+zebra show users
+zebra show registrations
+zebra show networks
+zebra show switches
+zebra show racks
+zebra show labs
+zebra show data-centers
+zebra command will use the private RSA key that the user created
+before registration and authenticate so there is no need to login for the CLI
+all show commands will support label filters
+*/
+
+// create inventory of commands for the zebra cli.
+func NewZebra() *cobra.Command {
+	// default zebra command to show resources.
+	zebraCmd := &cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:   "show",
+		Short: "show resources",
+	}
+
+	usrCmd := &cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "users",
+		Short:        "show zebra userss",
+		RunE:         ShowUsr,
+		Args:         cobra.MaximumNArgs(1),
+		SilenceUsage: true,
+	}
+
+	usrCmd.AddCommand(&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "regs",
+		Short:        "show zebra registrations",
+		RunE:         ShowReg,
+		SilenceUsage: true,
+	})
+
+	zebraCmd.AddCommand(usrCmd)
+
+	return zebraCmd
+}
+
+// function to add show commands for network resources to zebraCmd.
+func NewNetCmd() *cobra.Command {
+	zebraCmd := NewZebra()
+
+	zebraCmd.AddCommand(
+		&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+			Use:          "vlan",
+			Short:        "show zebra switches",
+			RunE:         ShowVlan,
+			Args:         cobra.MaximumNArgs(1),
+			SilenceUsage: true,
+		})
+
+	zebraCmd.AddCommand(&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "switches",
+		Short:        "show zebra switches",
+		RunE:         ShowSw,
+		Args:         cobra.MaximumNArgs(1),
+		SilenceUsage: true,
+	})
+
+	zebraCmd.AddCommand(&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "IP-Pools",
+		Short:        "show zebra IP-Address-Pools",
+		RunE:         ShowIP,
+		Args:         cobra.MaximumNArgs(1),
+		SilenceUsage: true,
+	})
+
+	return zebraCmd
+}
+
+// function to add show commands for dc resources to zebraCmd.
+func NewDCCmd() *cobra.Command {
+	zebraCmd := NewZebra()
+
+	zebraCmd.AddCommand(&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "dc",
+		Short:        "show datacenters",
+		RunE:         ShowDC,
+		SilenceUsage: true,
+	})
+
+	zebraCmd.AddCommand(&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "labs",
+		Short:        "show labs",
+		RunE:         ShowLab,
+		Args:         cobra.MaximumNArgs(1),
+		SilenceUsage: true,
+	})
+
+	zebraCmd.AddCommand(&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "racks",
+		Short:        "show racks",
+		RunE:         ShowRack,
+		Args:         cobra.MaximumNArgs(1),
+		SilenceUsage: true,
+	})
+
+	zebraCmd.AddCommand(zebraCmd)
+
+	return zebraCmd
+}
+
+// function to add show commands for server resources to zebraCmd.
+func NewSrvCmd() *cobra.Command {
+	zebraCmd := NewZebra()
+
+	zebraCmd.AddCommand(&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "servers",
+		Short:        "show servers",
+		RunE:         ShowServ,
+		SilenceUsage: true,
+	})
+
+	zebraCmd.AddCommand((&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "esx",
+		Short:        "show esx-servers",
+		RunE:         ShowESX,
+		Args:         cobra.MaximumNArgs(1),
+		SilenceUsage: true,
+	}))
+
+	zebraCmd.AddCommand((&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "vcenters",
+		Short:        "show vcenters",
+		RunE:         ShowVC,
+		Args:         cobra.MaximumNArgs(1),
+		SilenceUsage: true,
+	}))
+
+	zebraCmd.AddCommand((&cobra.Command{ //nolint:exhaustruct,exhaustivestruct
+		Use:          "vm",
+		Short:        "show vms",
+		RunE:         ShowVC,
+		Args:         cobra.MaximumNArgs(1),
+		SilenceUsage: true,
+	}))
+
+	zebraCmd.AddCommand(zebraCmd)
+
+	return zebraCmd
+}
--- cli/key/session_user.go
+++
@@ -1,7 +0,0 @@
-package key
-
-import "fmt"
-
-func BadTest() {
-	fmt.Println("Nothing here")
-}
