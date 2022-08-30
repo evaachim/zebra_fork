6c7856e932b4dd5eba8ef8efd71494ffd6722927
Eva Achim <eva.achim@bison.howard.edu>
2022-08-30 17:02:51 -0400

    updated content of string
    Signed-off-by: Eva Achim <eva.achim@bison.howard.edu>

--- lease/notif.go
+++ lease/notif.go
@@ -1,30 +1,31 @@
 package lease
 
-import "net/smtp"
+import (
+	"net/smtp"
+)
 
 func (l *Lease) Notify() error {
-	from := "admin@zebra.io"
+	from := " admin@zebra.project-safari.io"
 	pwd := "admin123"
 
 	person := l.Status.UsedBy
 
-	to := "<" + person + ">"
-
-	receiver := []string{to}
+	receiver := []string{
+		person,
+	}
 
 	host := "smtp.gmail.com"
 	port := "587"
 	addr := host + ":" + port
 
-	subject := "Lease request fulfilled."
-	body := "The lease request for " + l.Type + " for lease ID: " + l.ID + " is ready to be used."
+	subject := "Lease request fulfilled. "
+	body := "\nThe lease request for " + l.Type + " for lease ID: " + l.ID + " is ready to be used."
 
 	notification := []byte(subject + body)
 
 	auth := smtp.PlainAuth("", from, pwd, host)
 
 	err := smtp.SendMail(addr, auth, from, receiver, notification)
-
 	if err != nil {
 		panic(err)
 	}
