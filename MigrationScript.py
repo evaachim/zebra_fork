7e1bcd25a947b703338c9664acc0cce44531951b
Eva Achim <eva.achim@bison.howard.edu>
2022-10-09 16:29:29 -0400

    Add more ideas to preliminary
    Signed-off-by: Eva Achim <eva.achim@bison.howard.edu>

--- MigrationScript.py
+++ MigrationScript.py
@@ -2,6 +2,9 @@
 ##
 ## This could be extended for any type of resource for which there is data in the racktables database.
 
+## Are we updating the zebra tool to have this data statically and then access it via the api calls? 
+## Or we using the api calls to fetch from the db only that data which we need for the specific call?
+
 ## imports needed for access to mysql and the racktables database.
 import os
 import re
@@ -23,11 +26,33 @@ query = "/api/v1/resources"
 posts = "/api/v1/resources"
 delete = "/api/v1/resources"
 
+'''
+## To be used if second implementation option is selected 
+
 ## get the response for the query.
 queryResponse = requests.get(query)
 
 ## Make the response into json format.
 criteria = queryResponse.json()
+
+print(criteria)
+'''
+queryResponse = requests.get(query)
+## This contains the id to look for when querring.
+## Querry can be of different types - by id, by type, by label. These are in api.go.
+criteriaID = queryResponse.json()["id"]
+criteriaType = queryResponse.json()["type"]
+criteriaLabel = queryResponse.json()["label"]
+
+key = " "
+if len(criteriaID) != 0:
+    key = criteriaID
+elif len(criteriaType) !=0:
+    key = criteriaType
+elif len(criteriaLabel) != 0:
+    key = criteriaLabel
+else: key = ""
+
 
 ## Make the connection to the data base.
 connection = database.connect(
@@ -43,12 +68,13 @@ cursor = connection.cursor()
 ## Get data from database.
 
 ## Get all data for datacenter resources.
-def get_db_resource_data(criteria):
+## Key must be the one the user passes in when the query is made. 
+def get_db_resource_data(key):
     dbName, row, location = ""
     try:
         statement = "SELECT name, row_name, row_id, location_name FROM rackobject WHERE id=%s"
         ## selection is now by the object's id, it could be something else, as needed.
-        data = (criteria,)
+        data = (key,)
         cursor.execute(statement, data)
         for (name, row_name, row_id, location_name) in cursor:
             print("Retreived the data")
@@ -69,5 +95,21 @@ def get_db_resource_data(criteria):
 
 
 ## Get data for compute resources.
-
 ## get_compute_resource_data() should go here.
+## We don't have all of this data in the db, but it should be something like this:
+def get_compute_resource_data(key):
+    dbName, row, location = ""
+    try:
+        statement = "SELECT name, serial, model, ip, location_name FROM rackobject WHERE id=%s"
+        ## selection is now by the object's id, it could be something else, as needed.
+        data = (key,)
+        cursor.execute(statement, data)
+        for (name, serial, model, ip, location_name) in cursor:
+            print("Retreived the data")
+            srvName = name
+          ## row = row_name
+          ##  rowID = row_id
+            location = location_name
+    except database.Error as e:
+        print(f"Error retreiving entry from database: {e}")
+    return (srvName, serial, model, ip, location_name)
